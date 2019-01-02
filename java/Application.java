30fea31688480b4b8cda789fc9ff7c538267012b
vasavi vempati <vasavivempati2@gmail.com>
2019-01-02 14:42:40 -0800

    changing functions:

--- java/Application.java
+++ java/Application.java
@@ -2,6 +2,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
+import com.opencsv.CSVWriter;
 
 /**
  * Created by vasavivempati on 12/27/18.
@@ -28,8 +30,8 @@ public class Application {
                 //FileReader file = new FileReader(name);
                 BufferedReader br=new BufferedReader(new FileReader(name));
                 ////BufferedWriter writer = new BufferedWriter(new FileWriter("output.csv"));
-                ////FileWriter outputFile = new FileWriter("output.csv");
-                //CSVWriter fileWriter = new CSVWriter(outputFile);
+                FileWriter outputFile = new FileWriter("output.csv");
+                CSVWriter fileWriter = new CSVWriter(outputFile);
                 //ArrayList<String[]> data = new ArrayList<>();
 
                 while((line =br.readLine())!=null){
@@ -40,10 +42,18 @@ public class Application {
                     //fileWriter.writeNext(split);
 
                 }
+                processor.dataProcessor();
+                for (ArrayList<String> row: processor.getFull()) {
+                    String[] rowIn = new String[row.size()];
+                    int i =0;
+                    for (String item: row) { rowIn[i] = item; i++;}
+                    fileWriter.writeNext(rowIn);
+                }
 
-                //fileWriter.close();
+                fileWriter.close();
                 br.close();
             }
+
             catch(IOException e) {
                 System.out.println(e.getMessage());
             }
--- java/ReadInput.java
+++ java/ReadInput.java
@@ -3,18 +3,46 @@
  */
 import java.io.*;
 import java.text.Normalizer;
+import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Locale;
-
+import java.time.LocalDate;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.util.TimeZone;
+import java.util.Date;
 import static java.text.Normalizer.Form.NFD;
 
 public class ReadInput {
     ArrayList<String[]> outputContainer = new ArrayList<String[]>();
-    ArrayList<String[]> formattedOutputContainer = new ArrayList<String[]>();
+    ArrayList<String> formattedOutputContainer = new ArrayList<String>();
+    ArrayList<ArrayList<String>> full = new ArrayList<ArrayList<String>>();
+    long fooDuration = 0;
+    long barDuration = 0;
+    long totalDuration = 0;
     public  void addData(String[] input) throws IOException{
         outputContainer.add(input);
+    }
+    public ArrayList<String[]> getOutputContainer(){
+        return outputContainer;
+    }
+    public long getFooDuration()
+    {
+        return fooDuration;
+    }
+    public long getBarDuration()
+    {
+        return barDuration;
+    }
+    public long getTotalDurationDuration()
+    {
+        return totalDuration;
+    }
+    public  ArrayList<ArrayList<String>> getFull()
+    {
+        return full;
     }
     //call timestamp
     // call zip_code
@@ -25,19 +53,21 @@ public class ReadInput {
     // call Notes
     //using output container gotta normalize the data
     public void dataProcessor(){
-        for (String[] row:outputContainer) {
-
+        for (String[] column:outputContainer) {
+            rowProcessor(column);
         }
     }
     public void rowProcessor(String[] column ){
-        normalizeTimestamp(column[0]);
-        normalizeAddress(column[1]);
-        normalizeZipCode(column[2]);
-        //normalizeFullName(column[3]);
-       // normalizeFooDuration(column[4]);
-        //normalizeBarDuration(column[5]);
-        //normalizeTotalDuration(column[6]);
-        //normalizeNotes(column[7]);
+        SimpleDateFormat sdf = new SimpleDateFormat("d MMMM YYYY");
+        formattedOutputContainer.add(sdf.format(normalizeTimestamp(column[0])));
+        formattedOutputContainer.add(normalizeAddress(column[1]));
+        formattedOutputContainer.add( normalizeZipCode(column[2]));
+        formattedOutputContainer.add(normalizeFullName(column[3]));
+        formattedOutputContainer.add(Long.toString(normalizeFooDuration(column[4])));
+        formattedOutputContainer.add(Long.toString(normalizeBarDuration(column[5])));
+        formattedOutputContainer.add(Long.toString(normalizeTotalDuration()));
+        formattedOutputContainer.add(normalizeNotes(column[7]));
+        full.add(formattedOutputContainer);
     }
     private LocalDateTime normalizeTimestamp(String inputDate){
 
@@ -56,17 +86,34 @@ public class ReadInput {
     private String normalizeFullName(String fullName){
         return fullName.toUpperCase(Locale.ENGLISH);
     }
-    private void normalizeFooDuration(String foodDuration){
-        
+    private long normalizeFooDuration(String fooDurationInput){
+        try {DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("PST"));
+        Date date = dateFormat.parse((String.valueOf(fooDurationInput)));
+       fooDuration = date.getTime() / 1000L;
+       return fooDuration;}
+        catch(ParseException e) {
+            System.out.println(e.getMessage());
+        }
+        return fooDuration;
 
     }
-    private void normalizeBarDuration(String barDuration){
-
+    private long normalizeBarDuration(String barDurationInput){
+        try {DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("PST"));
+        Date date = dateFormat.parse((String.valueOf(barDurationInput)));
+        barDuration = date.getTime() / 1000L;
+        return barDuration;}
+        catch(ParseException e){
+            System.out.println(e.getMessage());
+        }
+        return barDuration;
     }
-    private void normalizeTotalDuration(String totalDuration){
-
+    private long normalizeTotalDuration(){
+        totalDuration = fooDuration + barDuration;
+        return totalDuration;
     }
-    private void normalizeNotes(String normalizeNotes){
-
+    private String normalizeNotes(String normalizeNotes){
+        return Normalizer.normalize(normalizeNotes,NFD);
     }
 }
